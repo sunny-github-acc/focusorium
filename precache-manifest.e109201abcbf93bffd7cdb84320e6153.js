f3a8f10a60122da8d346603ae279fd74588e2dbf
sunny <sunny@sunny.com>
2020-10-20 22:31:25 +0300

    Updates

---
+++ precache-manifest.e109201abcbf93bffd7cdb84320e6153.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbef4a0fbef3d2d9ff8319237d1200c3",
+    "url": "/focusorium/index.html"
+  },
+  {
+    "revision": "358a4f479cb142452aaf",
+    "url": "/focusorium/static/css/main.82ac92d6.chunk.css"
+  },
+  {
+    "revision": "7ce0c67be45372bac01a",
+    "url": "/focusorium/static/js/2.61545c6f.chunk.js"
+  },
+  {
+    "revision": "2c002d753ff02c59ef72251489c1ccff",
+    "url": "/focusorium/static/js/2.61545c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "358a4f479cb142452aaf",
+    "url": "/focusorium/static/js/main.aab8d217.chunk.js"
+  },
+  {
+    "revision": "c7cb2f672c424567c1f3",
+    "url": "/focusorium/static/js/runtime-main.8ee7b1bf.js"
+  }
+]);
